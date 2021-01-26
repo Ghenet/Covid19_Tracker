65ad7ecfee8cb6215151a93848e367b4d6825a06
Ghenet Andemiachael <ghenikibrom@gmail.com>
2021-01-26 13:59:12 -0800

    fetches te daily data of the us and displays it in line chart

--- src/api/index.js
+++ src/api/index.js
@@ -1,48 +1,41 @@
+  
 import axios from 'axios';
-import CountryPicker from '../components/CountryPicker/CountryPicker';
 
 const url = 'https://covid19.mathdro.id/api';
 
-// /------------------------Cards-------------------/
-
 export const fetchData = async (country) => {
-    let changeableUrl = url;
+  let changeableUrl = url;
 
-    if (country) {
-        changeableUrl = `${url}/countries/${country}`
-    }
-    try {
-        //we can say {data} instead of response.data
-        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableUrl);
+  if (country) {
+    changeableUrl = `${url}/countries/${country}`;
+  }
 
-        const modifiedData = { confirmed, recovered, deaths, lastUpdate }
+  try {
+    const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableUrl);
 
-        return modifiedData;
-    } catch (error) {
+    return { confirmed, recovered, deaths, lastUpdate };
+  } catch (error) {
+    return error;
+  }
+};
 
-    }
-}
-
-
-
-// ------------------------Charts--------------------------
+// Instead of Global, it fetches the daily data for the US
 export const fetchDailyData = async () => {
     try {
-        const { data } = await axios.get(`${url}/daily`);
-    return data.map(({ confirmed, deaths, reportDate: date }) => ({ confirmed: confirmed.total, deaths: deaths.total, date }));
-  } catch (error) {
-  return error;
- }
-}
-
-
-// -------------------------CountryPicker------------------------
-export const fetchCountries = async () => {
-    try {
-        const { data: { countries } } = await axios.get(`${url}/countries`);
-
-        return countries.map((country) => country.name);
+      const { data } = await axios.get('https://api.covidtracking.com/v1/us/daily.json');
+  
+      return data.map(({ positive, recovered, death, dateChecked: date }) => ({ confirmed: positive, recovered, deaths: death, date }));
     } catch (error) {
-        console.log(error);
+      return error;
     }
-}
+  };
+
+export const fetchCountries = async () => {
+  try {
+    const { data: { countries } } = await axios.get(`${url}/countries`);
+
+    return countries.map((country) => country.name);
+  } catch (error) {
+    return error;
+  }
+};
